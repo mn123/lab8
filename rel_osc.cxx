b82cea1092f9aa6efbda6a4429b970a028f5c8b8
Matthias Neufeld <maneuf@pc10.pool.tp1.uni-duesseldorf.de>
2015-12-15 15:46:04 +0100

    lab8

--- rel_osc.cxx
+++ rel_osc.cxx
@@ -6,8 +6,8 @@
 #include <iostream>
 #include <fstream>
 //--------------------
-void f(double* const y0, const double x);
-void RKstep(double* const yn, const double* const y0, const double x, const double dx);
+void f(double* const y0, const double t);
+void RKstep(double* const yn, const double* const y0, const double t, const double dt, double* k1, double* k2, double* k3, double* k4, const int dim);
 //--------------------
 using namespace std;
 //--------------------
@@ -15,55 +15,76 @@ using namespace std;
 int main(void)
 {
 	ofstream out("solution");
-  const int dim = 3;
-	double dx = 0.001,x=0;
-	const double L = 100;
-  double y0[dim] = {1.01, 1.0, 1.0};
+  const int dim = 2;
+	double t, p0;
+	const double L = 20, dt = 0.15;
+for (p0=0; p0<5; p0 +=0.01){
+  t=0;
+  double y0[dim] = {p0, 0};
 	double yn[dim];
-
-  out << x << "\t" << y0[0] << "\t" << y0[1] << "\t" << y0[2] << endl;
-	while(x<=L)
+  double k1[dim], k2[dim], k3[dim], k4[dim];
+  out << t << "\t" << y0[0] << "\t" << y0[1] << endl;
+	while(t<=L)
 	{
-		x += dx;
-		RKstep(yn, y0, x, dx);
-    for(int i=0; i<dim; i++) y0[i] = yn[i];
-		out << x << "\t" << y0[0] << "\t" << y0[1] << "\t" << y0[2] << endl;
+		t += dt;
+		RKstep(yn, y0, t, dt,k1,k2,k3,k4,dim);
+		if (y0[1]>0 && yn[1]<0)
+		  break;
+		
+		  
+                for(int i=0; i<dim; i++) y0[i] = yn[i];
+		out << t << "\t" << y0[0] << "\t" << y0[1] << endl;
 	}
+	
+	double lth=0, rth=1, th;
+	double b1,b2,b3,b4;
+	t -= dt;
+	for (int i=0; i<10; i++){
+	  th=(lth+rth)/2.0;
+	  b1=th-3*th*th/2.0+2*th*th*th/3.0;
+	  b2=th*th-2*th*th*th/3.0;
+	  b3=b2;
+	  b4=-th*th/2.0+2*th*th*th/3.0;
+	  //yn[0]=y0[0]+dt*(b1*k1[0]+b2*k2[0]+b3*k3[0]+b4*k4[0]);
+	  yn[1]=y0[1]+dt*(b1*k1[1]+b2*k2[1]+b3*k3[1]+b4*k4[1]);
+	
+	  if (yn[1]>0)
+	    lth=th;
+	  else rth=th;
+	}
+	out << t+dt*th << "\t" << yn[0] << "\t" << yn[1] << endl;
 	out.close();
+	cout << p0 << "\t" << t+dt*th << endl;
+  
+}
 	return(0);
 }
 //-------------------
 void RKstep(double* const yn, const double* const y0,
-            const double x, const double dx)
+            const double t, const double dt, double* k1, double* k2, double* k3, double* k4, const int dim)
 {
-	const int dim = 3;
-	double k1[dim], k2[dim], k3[dim], k4[dim];
+	for(int i=0;i<dim; i++) k1[i] = y0[i];
+	f(k1, t);
 
-  for(int i=0;i<dim; i++) k1[i] = y0[i];
-	f(k1, x);
+	for(int i=0;i<dim; i++) k2[i] = y0[i] + 0.5 * dt * k1[i];
+	f(k2, t+0.5*dt);
 
-	for(int i=0;i<dim; i++) k2[i] = y0[i] + 0.5 * dx * k1[i];
-  f(k2, x+0.5*dx);
+	for(int i=0;i<dim; i++) k3[i] = y0[i] + 0.5 * dt * k2[i];
+	f(k3, t+0.5*dt);
 
-	for(int i=0;i<dim; i++) k3[i] = y0[i] + 0.5 * dx * k2[i];
-	f(k3, x+0.5*dx);
-
-  for(int i=0;i<dim; i++) k4[i] = y0[i] + dx * k3[i];
-	f(k4,  x+dx);
+	for(int i=0;i<dim; i++) k4[i] = y0[i] + dt * k3[i];
+	f(k4,  t+dt);
 
 	for(int i=0;i<dim; i++)
-	 yn[i] = y0[i] + 1./6.*dx*(k1[i] + 2*k2[i] + 2*k3[i] + k4[i]);
+	yn[i] = y0[i] + 1./6.*dt*(k1[i] + 2*k2[i] + 2*k3[i] + k4[i]);
 }
 //-------------------
 // Lorenz model
-void f(double* const y0, const double x)
+void f(double* const y0, const double t)
 {
-	const double a = 10;
-	const double b = 28;
-	const double c = 8.0/3.0;
-	double y[3] = { y0[0], y0[1], y0[2] };
+	double y[2] = { y0[0], y0[1] };
 
-  y0[0] = a*(y[1] - y[0]);
-	y0[1] = y[0]*(b - y[2]) - y[1];
-	y0[2] = y[0]*y[1] - c*y[2];
+	y0[0] = y0[1];
+	y0[1] = -y[0]/sqrt(1+y[0]*y[0]);
+
 }
